880d161036e16e5a3f935c36437e0a3ba04f04b2
Ashlee Barksdale <ashleelennab@gmail.com>
2020-07-05 02:53:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,11 @@
 def reverse_each_word(string)
-  string.split.collect do |word| 
-    word = word.reverse 
-    
-  end 
-  .join(" ")
+  results = []
+  string.split.each do |word|
+    results.append(word.reverse)
+  end
+  results.join(" ")
+  
+  #string.split.collect do |word| 
+  #  word = word.reverse 
+  #end.join(" ")
 end
